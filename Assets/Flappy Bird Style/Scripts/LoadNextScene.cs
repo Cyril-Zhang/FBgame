dfe29b1dc5cf825bb89a8c9a3e520b5557baec27
Zhangsiyu <zhangsiyu1107@gmail.com>
2020-09-26 16:47:32 -0500

    Create repo

---
+++ Assets/Flappy Bird Style/Scripts/LoadNextScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadNextScene : MonoBehaviour
+{
+    void Start()
+    {
+        print(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void TaskOnClick()
+    {
+        Application.LoadLevel(1);
+    }
+}
