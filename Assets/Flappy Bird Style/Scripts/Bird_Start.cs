72596043b72522ed3f2f309738b41730b7a08cd3
Zhangsiyu <zhangsiyu1107@gmail.com>
2020-09-26 23:12:42 -0500

    Start scene change

--- Assets/Flappy Bird Style/Scripts/Bird_Start.cs
+++ Assets/Flappy Bird Style/Scripts/Bird_Start.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Bird_Start : MonoBehaviour
 {
-	public float upForce;                   //Upward force of the "flap".
-	private bool isDead = false;            //Has the player collided with a wall?
-
+	public float upForce = 200;                   //Upward force of the "flap".
+	private int cout = 0;
+	private float bottom_line;
 	private Animator anim;                  //Reference to the Animator component.
 	private Rigidbody2D rb2d;               //Holds a reference to the Rigidbody2D component of the bird.
 
@@ -16,36 +16,33 @@ public class Bird_Start : MonoBehaviour
 		anim = GetComponent<Animator>();
 		//Get and store a reference to the Rigidbody2D attached to this GameObject.
 		rb2d = GetComponent<Rigidbody2D>();
+		this.bottom_line = Random.Range(-1.5f, 1.5f);
 	}
 
 	void Update()
 	{
-		//Don't allow control if the bird has died.
-		if (isDead == false)
+		if (transform.position.y < this.bottom_line)
 		{
-			//Look for input to trigger a "flap".
-			if (Input.GetMouseButtonDown(0))
-			{
-				//...tell the animator about it and then...
-				anim.SetTrigger("Flap");
-				//...zero out the birds current y velocity before...
-				rb2d.velocity = Vector2.zero;
-				//	new Vector2(rb2d.velocity.x, 0);
-				//..giving the bird some upward force.
-				rb2d.AddForce(new Vector2(0, upForce));
-			}
+			flappy();
+        }
+        else
+        {
+			this.cout = 0;
+			this.bottom_line = Random.Range(-1.5f, 1.5f);
 		}
 	}
-
-	void OnCollisionEnter2D(Collision2D other)
-	{
-		// Zero out the bird's velocity
-		rb2d.velocity = Vector2.zero;
-		// If the bird collides with something set it to dead...
-		isDead = true;
-		//...tell the Animator about it...
-		anim.SetTrigger("Die");
-		//...and tell the game control about it.
-		GameControl.instance.BirdDied();
+	void flappy()
+    {
+		if(this.cout < 1)
+        {
+				//...tell the animator about it and then...
+			anim.SetTrigger("Flap");
+				//...zero out the birds current y velocity before...
+			rb2d.velocity = Vector2.zero;
+				//	new Vector2(rb2d.velocity.x, 0);
+				//..giving the bird some upward force.
+			rb2d.AddForce(new Vector2(0, upForce));
+			this.cout++;
+		}
 	}
 }
